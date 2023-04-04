ea844c1700b3598c33e3af5b5a39002d3ab07de4
BaBa0525 <hsunhsun0525@gmail.com>
2023-04-04 23:30:37 +0800

    🔧 Add font-family and navbar color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navbar: "#0269a6",
+      },
+      fontFamily: {
+        "roboto-mono": ["var(--font-roboto-mono)", ...fontFamily.mono],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
