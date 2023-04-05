27ce9b7cd6f7f9ab2dddd7400fc58ca08ebf15ce
BaBa0525 <hsunhsun0525@gmail.com>
2023-04-05 13:40:49 +0800

    💄 Make navbar higher

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { type FC } from "react";
 
 export const Navbar: FC = () => {
   return (
-    <nav className="fixed flex h-12 w-full items-center bg-navbar">
+    <nav className="fixed z-10 flex h-12 w-full items-center bg-navbar">
       <h1 className="m-3 font-roboto-mono text-2xl font-semibold text-white">
         Dcard Tools
       </h1>
